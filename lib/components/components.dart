80f51de1556c85c54438ccf80dd1bd98cd02c8a6
classic-code <amitech125@gmail.com>
2022-03-26 09:50:10 +0100

    update

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,14 +1,11 @@
-import 'package:digs_sanctuary_api/digs_sanctuary_api.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
 part 'data_picker.dart';
 part 'indicator.dart';
 part 'line.dart';
-part 'list_picker.dart';
 part 'loader.dart';
 part 'text_field.dart';
 part 'toast_snackbar.dart';
